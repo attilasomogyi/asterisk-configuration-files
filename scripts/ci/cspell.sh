777733412fc8435b4e4fad4527940ed93c5d7637
Attila Zsolt Somogyi <attila.zsolt.somogyi@gmail.com>
2023-03-12 17:18:19 +0100

    ci: fix cspell script
    fix cspell script because not handled the dependences

--- scripts/ci/cspell.sh
+++ scripts/ci/cspell.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env sh
+set -e
+dev_null=/dev/null
+which node >$dev_null || {
+    echo "node not found"
+    exit 1
+}
+which npm >$dev_null || {
+    echo "npm not found"
+    exit 1
+}
+which yarn >$dev_null || npm install --global yarn
+which cspell >$dev_null || yarn global add cspell
 cspell lint --fail-fast .
