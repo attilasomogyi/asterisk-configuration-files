63b983420b7aa7297d2fcd5f1a104af59e532fcd
Attila Zsolt Somogyi <attila.zsolt.somogyi@gmail.com>
2023-04-07 14:53:19 +0200

    test: fix vale setup script

--- scripts/vale-setup.sh
+++ scripts/vale-setup.sh
@@ -2,19 +2,8 @@
 
 set -e
 
-function edit_yaml() {
-  yaml_file="$1"
-  yq -i '.level = "error"' "$yaml_file"
-}
-
 function main() {
   vale sync || exit 1
-  styles=$(git ls-files --ignored --exclude /styles/ --other -- "*.yml")
-
-  export -f edit_yaml
-  if [ -n "$styles" ]; then
-    printf '%s\n' "$styles" | xargs -n1 -P "$(nproc)" bash -c 'edit_yaml "$@"' _
-  fi
 }
 
 if [ "$(basename "$0")" != "bash" ]; then
